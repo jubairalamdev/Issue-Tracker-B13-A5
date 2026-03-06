dd2a6eca872f814491ed9389c6fa811840920dfa
Jubair Alam <jubairalam.dev@gmail.com>
2026-03-07 02:54:50 +0600

    Login Done and Main Page Started Desiging.. 2 sections done including header

---
+++ Scripts/login.js
@@ -0,0 +1,23 @@
+// console.log("YEP IM WORKING RN")
+const domLoad = () => {
+    const errorText = document.getElementById("error-text");
+    errorText.classList.add("hidden");
+}
+
+const login = () => {
+    const errorText = document.getElementById("error-text");
+    if (errorText.classList.contains("block")) {
+        errorText.classList.replace("block", "hidden");
+    }
+    else {
+        errorText.classList.add("hidden");
+    }
+    const username = document.getElementById("usernameInput");
+    const password = document.getElementById("passwordInput");
+    if (username.value === "admin" && password.value === "admin123") {
+        window.location.assign("./main.html");
+    }
+    else {
+        errorText.classList.replace("hidden", "block");
+    }
+}
